1237c7552406b473c04aa7d6cc983f372e5dfd5c
Morteza Allahpour <mortezaallahpour@gmail.com>
2018-05-23 12:45:14 +0430

    see changes via diff

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,15 +3,16 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Morteza Allahpour'
-SITENAME = 'My Blogs'
+SITENAME = ''
 SITEURL = ''
-
+USE_FOLDER_AS_CATEGORY = True
+DELETE_OUTPUT_DIRECTORY = True
 PATH = 'content'
 
 TIMEZONE = 'Asia/Tehran'
-
+DEFAULT_DATE = 'fs'
 DEFAULT_LANG = 'fa'
-
+DISPLAY_PAGES_ON_MENU = True
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -20,16 +21,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+	('Twitter', 'https://twitter.com/#'),
+	)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter/#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+THEME = './pelican-semantic-ui'
--- publishconf.py
+++ publishconf.py
@@ -10,6 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# SITEURL = 'https://mortza.github.io'
 SITEURL = ''
 RELATIVE_URLS = False
 
